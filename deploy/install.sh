41ce7b493758085e8f398babde7942e2cbd59c26
Vasyl Saienko <vsaienko@mirantis.com>
2026-01-19 15:34:43 +0200

    Set randoms passwords during install

--- deploy/install.sh
+++ deploy/install.sh
@@ -13,6 +13,15 @@ if [[ ! -d $NETRIS_SIMULATOR_DIR ]]; then
     git clone https://github.com/jumpojoy/netris-simulator $NETRIS_SIMULATOR_DIR
 fi
 
+CUMULUS_NEW_PASSWORD=$(date +%s | sha256sum | base64 | head -c 15)
+NETRIS_ADMIN_PASSWORD=$(date +%s | sha256sum | base64 | head -c 15)
+
+sed -ie "s/<CUMULUS_NEW_PASSWORD>/${CUMULUS_NEW_PASSWORD}/g" ${NETRIS_SIMULATOR_DIR}/inventory.yml
+sed -ie "s/<NETRIS_ADMIN_PASSWORD>/${NETRIS_ADMIN_PASSWORD}/g" ${NETRIS_SIMULATOR_DIR}/inventory.yml
+
 
 ansible-playbook -i ${NETRIS_SIMULATOR_DIR}/inventory.yml ${NETRIS_SIMULATOR_DIR}/k0s.yml
 ansible-playbook -i ${NETRIS_SIMULATOR_DIR}/inventory.yml ${NETRIS_SIMULATOR_DIR}/configure-sushyemulator.yml
+
+
+ansible-playbook -i ${NETRIS_SIMULATOR_DIR}/inventory.yml ${NETRIS_SIMULATOR_DIR}/create-vms.yml
